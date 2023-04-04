8ddc3fd51a1a7f33ed324e51bfbaa8a61ad1cbc1
Josey Sekine <ipho.7johero@icloud.com>
2023-04-04 14:40:04 -0700

    position of the button has been changed. effects for the cards are changed.

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,18 +1,50 @@
 import React from 'react';
 import image from '../img/retro_bg_17.png'
+import Carousel from 'react-bootstrap/Carousel';
 
 
 function TopBody(){
   return (
-    <div className="body-contents" style={{ backgroundImage: `url(${image})`}}>
-      <h3>Welcome to my page</h3>
-      <div className="nav-sub-contents">
-        <h4>Github</h4>
-        <h4>Linkedin</h4>
-        <h4>Websites</h4>
-      </div>
-    </div>
-  )
+    <Carousel interval={null}>
+      <Carousel.Item >
+        <img
+          className="d-block w-100"
+          src={image}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://png.pngtree.com/thumb_back/fh260/background/20210324/pngtree-portfolio-memphis-playful-abstract-pink-image_593412.jpg"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpWgDWHr5t4bYyv_eKUS9MyMIG8_9-seHJ_w&usqp=CAU"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
 
